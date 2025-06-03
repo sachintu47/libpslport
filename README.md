05a44928e31074c3dc4b70e4b8b3af587a2f2328
Igor Todorovski <itodorov@ca.ibm.com>
2025-06-03 16:11:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/zopencommunity/libpslport/actions/workflows/bump.yml/badge.svg)](https://github.com/zopencommunity/libpslport/actions/workflows/bump.yml)
+
+# libpsl
+
+C library for the Public Suffix List
---
+++ cicd-dev.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/libpslport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libpslport.git'), string(name: 'PORT_DESCRIPTION', value: 'C library for the Public Suffix List' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+  }
+}
